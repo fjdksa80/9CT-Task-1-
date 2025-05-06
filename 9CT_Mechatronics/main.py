5d8bcf24674042f25cd0f1de2e547b0d5375bf1a
fjdksa80 <fraser.maple431@education.nsw.gov.au>
2025-05-06 11:07:43 +1000

    Update main.py
    coding robot

--- 9CT_Mechatronics/main.py
+++ 9CT_Mechatronics/main.py
@@ -7,14 +7,21 @@ from pybricks.tools import wait, StopWatch, DataLog
 from pybricks.robotics import DriveBase
 from pybricks.media.ev3dev import SoundFile, ImageFile
 
-
-# This program requires LEGO EV3 MicroPython v2.0 or higher.
-# Click "Open user guide" on the EV3 extension tab for more information.
-
-
-# Create your objects here.
+# Variables
 ev3 = EV3Brick()
+left_motor = Motor(Port.B)
+right_motor = Motor(Port.C)
+robot = DriveBase(left_motor, right_motor, wheel_diameter=55.5, axle_track=104)
+colour_sensor = ColorSensor(Port.S3)
+ultrasonic_sensor = UltrasonicSensor(Port.S2)
+colour_sensor_motor = Motor(Port.A)
 
-
-# Write your program here.
-ev3.speaker.beep()
+# Main loop
+while True:
+    while ultrasonic_sensor > 30:
+        robot.drive(20, 0)
+    robot.stop()
+    colour_sensor_motor.run_angle(90, 90)
+    if colour_sensor.Color() == Colour.RED:
+        ev3.speaker.beep()
+    
